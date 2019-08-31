89d1eb976103d11fd96d919780f2fdd23d8e76e0
Todd Gamblin <tgamblin@llnl.gov>
2019-08-31 14:15:30 -0700

    Add another loc example

--- pandas/loc.py
+++ pandas/loc.py
@@ -4,9 +4,11 @@ import numpy as np
 import pandas as pd
 import string
 
+letters = [c for c in string.ascii_lowercase]
+
 df = pd.DataFrame(
     {
-        "alpha": [c for c in string.ascii_lowercase],
+        "alpha": letters,
         "c1": range(1 * 26),
         "c2": range(1 * 26, 2 * 26),
         "c3": range(2 * 26, 3 * 26),
@@ -37,3 +39,6 @@ cols = ["c2", "c4"]
 df.loc["a", cols] = np.sum(df.loc[rows, cols])
 print(df)
 print()
+
+
+print(df.loc[letters, ["c2", "c3", "c4"]] > 5.0)
